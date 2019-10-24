f5a9f6b6823e34ed7459c06b8ec6fd58692c1a2b
ydvaman <2017pec5250@mnit.ac.in>
2019-10-24 15:38:18 +0530

    sd

--- DemoGit/FunctionApp1/Function1.cs
+++ DemoGit/FunctionApp1/Function1.cs
@@ -27,7 +27,7 @@ namespace FunctionApp1
 
             return name != null
                 ? (ActionResult)new OkObjectResult($"Hello, {name}")
-                : new BadRequestObjectResult("Please pass a name on the query string or in the request body");
+                : new BadRequestObjectResult("My Name is Khan");
         }
     }
 }
